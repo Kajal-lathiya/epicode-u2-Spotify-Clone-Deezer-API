82c7b4f384ed9599a5d116bd9da6900654c906f3
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-11-16 10:58:43 +0100

    agathas work added

--- home.js
+++ home.js
@@ -12,57 +12,15 @@ async function getAlbums() {
   );
 
   const listOfSearch = await response.json();
+  console.log(listOfSearch);
   return listOfSearch; //                                                       listOfSearch
 }
-
-async function getSongs() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a384224cb5msh269936e2552c740p19046ejsnc63fad635c19",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
-  const response = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=whatever`,
-    options
-  );
-
-  const listOfSongs = await response.json();
-  return listOfSongs;
-}
-
-async function getArtist() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a384224cb5msh269936e2552c740p19046ejsnc63fad635c19",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
-  const response = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=song`,
-    options
-  );
-
-  const listOfArtists = await response.json();
-  return listOfArtists;
-}
-
 window.onload = async () => {
   const listOfSearch = await getAlbums();
-  const listOfSongs = await getSongs();
-  const listOfArtists = await getArtist();
-  console.log(listOfArtists);
   console.log(listOfSearch);
-  console.log(listOfSongs);
   fillPageAlbums(listOfSearch);
-  fillPageSongs(listOfSongs);
-  fillPageArtists(listOfArtists);
 };
-
 const topsidecards = document.querySelector("#topsidecards");
-
 const fillPageAlbums = function (listOfSearch) {
   topsidecards.innerHTML = "";
   for (i = 0; i < 10; i++) {
