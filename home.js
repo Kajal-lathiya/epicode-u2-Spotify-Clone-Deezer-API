7b3d6011f8edbbdad4ff526de1fe699a9e111102
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-11-17 11:31:45 +0100

    added redirection to id's

--- home.js
+++ home.js
@@ -25,21 +25,23 @@ const fillPageAlbums = function (listOfSearchAlbums) {
   topsidecards.innerHTML = "";
   for (i = 0; i < 10; i++) {
     topsidecards.innerHTML += `<div class="col" id="top-buttons">
-  <div class="background-card mb-3" style="max-width: 540px">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img
-          src="${listOfSearchAlbums.data[i].album.cover_medium}"
-          class="img-fluid rounded-start aspect-ratio-one-to-one" style="width: 100%"
-          alt="..."
-        />
-      </div>
-      <div class="col-md-8 d-flex align-items-center">
-        <div class="card-body d-flex align-items-center justify-content-between">
-          <p class="card-title text-truncate">${listOfSearchAlbums.data[i].album.title}</p>
-          <i class="fa-solid fa-circle-play fa-2xl"></i>
+    <div class="background-card mb-3" style="max-width: 540px">
+     
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+                src="${listOfSearchAlbums.data[i].album.cover_medium}"
+                class="img-fluid rounded-start aspect-ratio-one-to-one" style="width: 100%"
+                alt="..."
+              />
+          </div>
+          <div class="col-md-8 d-flex align-items-center">
+          <div class="card-body d-flex align-items-center justify-content-between">
+              <p class="card-title text-truncate">${listOfSearchAlbums.data[i].album.title}</p>
+              <a href="/Album/album.html?songId=${listOfSearchAlbums.data[i].album.id}"><i class="fa-solid fa-circle-play fa-2xl"></i></a>
+          </div>
         </div>
-      </div>
+     
     </div>
   </div>
 </div>`;
@@ -121,13 +123,13 @@ const fillPageArtists = function ([
   //useless loop
   midsidecards.innerHTML = ` <div class="col">
     <div class="card" id="darker">
-      <img
+      <a href="/Artist/artist.html?songId=${listOfSearchArtists.id}"><img
         src="${listOfSearchArtists.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists.name}</h5>
         <div class="d-flex justify-content-between">
@@ -140,13 +142,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists2.id}"><img
         src="${listOfSearchArtists2.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists2.name}</h5>
         <p class="card-text">Lorem ipsum...</p>
@@ -156,13 +158,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists3.id}"><img
         src="${listOfSearchArtists3.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists3.name}</h5>
         <p class="card-text">Lorem ipsum... </p>
@@ -172,13 +174,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists4.id}"><img
         src="${listOfSearchArtists4.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists4.name}</h5>
         <p class="card-text">Lorem ipsum... </p>
@@ -188,13 +190,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists5.id}"><img
         src="${listOfSearchArtists5.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists5.name}</h5>
         <p class="card-text">Lorem ipsum... </p>
@@ -204,13 +206,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists6.id}"><img
         src="${listOfSearchArtists6.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists6.name}</h5>
         <p class="card-text">Lorem ipsum... </p>
@@ -220,13 +222,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists7.id}"><img
         src="${listOfSearchArtists7.picture_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists7.name}</h5>
         <p class="card-text">Lorem ipsum... </p>
@@ -236,13 +238,13 @@ const fillPageArtists = function ([
     `;
   midsidecards.innerHTML += ` <div class="col">
     <div class="card" id="darker">
-      <img
+    <a href="/Artist/artist.html?songId=${listOfSearchArtists8.id}"><img
         src="${listOfSearchArtists8.cover_xl}"
         class="img-fluid p-2"
         id="rounded"
         style="height: 100%; min-width: 100%;"
         alt="..."
-      />
+      /></a>
       <div class="card-body">
         <h5 class="card-title">${listOfSearchArtists8.title}</h5>
         <p class="card-text">Lorem ipsum... </p>
